3c33b53f1b3db7b444dfec6b7fcd350b169f44f0
Doug Pedley <dpedley@gmail.com>
2020-12-27 23:43:48 -0800

    Initial Commit

---
+++ stockfish/stockfish.h
@@ -0,0 +1,12 @@
+//
+//  stockfish.h
+//  stockfish
+//
+//  Created by Douglas Pedley on 12/27/20.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface stockfish : NSObject
+
+@end
---
+++ stockfish/stockfish.m
@@ -0,0 +1,12 @@
+//
+//  stockfish.m
+//  stockfish
+//
+//  Created by Douglas Pedley on 12/27/20.
+//
+
+#import "stockfish.h"
+
+@implementation stockfish
+
+@end
